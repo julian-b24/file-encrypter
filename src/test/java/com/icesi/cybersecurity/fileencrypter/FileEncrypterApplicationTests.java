2662e0c9e999c3ae6586a128936ff5d9391b7e74
julian-b24 <julian.camilob24@gmail.com>
2023-05-09 10:38:48 -0500

    Initial commit and spring project

---
+++ src/test/java/com/icesi/cybersecurity/fileencrypter/FileEncrypterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.cybersecurity.fileencrypter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileEncrypterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
