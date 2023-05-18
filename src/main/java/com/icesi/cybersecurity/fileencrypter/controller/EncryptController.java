29dc7ab16a9e9bac2512fe74d45f2f45bdddb196
julian-b24 <julian.camilob24@gmail.com>
2023-05-18 09:44:23 -0500

    Controller and service anottations added

--- src/main/java/com/icesi/cybersecurity/fileencrypter/controller/EncryptController.java
+++ src/main/java/com/icesi/cybersecurity/fileencrypter/controller/EncryptController.java
@@ -1,7 +1,11 @@
 package com.icesi.cybersecurity.fileencrypter.controller;
 
 import com.icesi.cybersecurity.fileencrypter.api.EncryptAPI;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
+@RestController
 public class EncryptController implements EncryptAPI {
 
     @Override
--- src/main/java/com/icesi/cybersecurity/fileencrypter/services/impl/EncryptServiceImpl.java
+++ src/main/java/com/icesi/cybersecurity/fileencrypter/services/impl/EncryptServiceImpl.java
@@ -1,8 +1,14 @@
 package com.icesi.cybersecurity.fileencrypter.services.impl;
 
 import com.icesi.cybersecurity.fileencrypter.services.EncryptService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class EncryptServiceImpl implements EncryptService {
+
+    EncryptService encryptService;
 
     @Override
     public void encryptFile() {
